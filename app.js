20be96ef47502600a4902eb27a3fe4ff4715540f
AndyCandy <56514575+lauhard@users.noreply.github.com>
2023-12-02 22:10:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(req, res){
-	res.write("resopnse3!");
+	res.write("the cron job pulls the code! yay"");
 	res.end();
 }).listen(3000);
 console.log("server started on port:",3000);
