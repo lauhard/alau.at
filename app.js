81b6ea3873561be3ca8bd54a473c8e612a005756
lauhard.andreas <lauhard.andreas@gmail.com>
2023-12-03 21:06:01 +0100

    changed server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(req, res){
-	res.write("the cron job pulls the code! yay!");
+	res.write("the cron job pulls the code! connected with client");
 	res.end();
 }).listen(3000);
 console.log("server started on port:",3000);
